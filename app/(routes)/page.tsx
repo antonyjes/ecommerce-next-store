3e62c80211a8a6f0d9d085b0188778452aaf357f
antonyjes <72809149@continental.edu.pe>
2023-07-23 21:12:07 -0500

    Fetch products

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,19 +1,25 @@
 import getBillboards from "@/actions/get-billboards";
+import getProducts from "@/actions/get-products";
 import Billboard from "@/components/billboard";
+import ProductList from "@/components/product-list";
 import Container from "@/components/ui/container";
 
 export const revalidate = 0;
 
-const HomePage = async() => {
-  const billboard = await getBillboards();
+const HomePage = async () => {
+  const billboards = await getBillboards();
+  const products = await getProducts({ isFeatured: true });
 
-  return(
+  return (
     <Container>
       <div className="space-y-10 pb-10">
-        <Billboard data={billboard[0]}/>
+        <Billboard data={billboards[0]} />
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Featured Products" items={products} />
+        </div>
       </div>
     </Container>
-  )
-}
+  );
+};
 
 export default HomePage;
